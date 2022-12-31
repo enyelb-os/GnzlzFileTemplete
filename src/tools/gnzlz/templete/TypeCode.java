4d4d4385e49653318732ee31eb281d3a42b79f1f
enyelb-os <enyelb.gnzlz@gmail.com>
2022-12-30 23:44:40 -0400

    update init

--- src/tools/gnzlz/templete/TypeCode.java
+++
@@ -1,9 +0,0 @@
-package tools.gnzlz.templete;
-
-import tools.gnzlz.templete.instruction.Type;
-
-public class TypeCode {
-    public static Type type(String code){
-        return Type.VAR;
-    }
-}
